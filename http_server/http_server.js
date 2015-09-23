cdb445c7143c3ea202e999f2be585803f7be9943
ms.oosumi <masaki.ohsumi.1021@outlook.jp>
2015-09-23 19:33:56 +0900

    add updateapi to rest

--- http_server/http_server.js
+++ http_server/http_server.js
@@ -2,6 +2,41 @@ var http = require('http');
 var url = require('url');
 var items = [];
 
+function getResultOfPath ( req ) {
+	var statusCode;
+	var text;
+	var path;
+
+	var path = url.parse(req.url).pathname;
+	var i = parseInt(path.slice(1), 10);
+
+	if( isNaN(i) ) {
+		statusCode = 400;
+		text = 'Invalid item id';
+	} else if( !items[i] ) {
+		statusCode = 400;
+		text = 'Item not found';
+	}else {
+		statusCode = 200;
+		text = 'OK\n';
+		path = i;
+	}
+	return {
+		statusCode: statusCode,
+		text: text,
+		path: path
+	};
+};
+function updateItems ( index, newValue ) {
+	var isUpdate = false;
+	if( !items[index] ) {
+		return isUpdate;
+	}
+	items[index] = newValue;
+	isUpdate = true;
+	return isUpdate;
+};
+
 var server = http.createServer(function (req, res) {
 	switch( req.method ) {
 		case 'POST':
@@ -24,19 +59,36 @@ var server = http.createServer(function (req, res) {
 			res.end(body);
 			break;
 		case 'DELETE':
-			var path = url.parse(req.url).pathname;
-			var i = parseInt(path.slice(1), 10);
-
-			if( isNaN(i) ) {
-				res.statusCode = 400;
-				res.end('Invalid item id');
-			} else if( !items[i] ) {
-				res.statusCode = 404;
-				res.end('Item not found');
-			}else {
-				items.splice(i, 1);
-				res.end('OK\n');
+			var result = getResultOfPath(req);
+			if( result.statusCode === 200 ) {
+				items.splice(result.path, 1);
 			}
+			res.statusCode = result.statusCode;
+			res.end(result.text);
+			break;
+		case 'PUT':
+			var result = getResultOfPath(req);
+			if( result.statusCode !== 200 ) {
+				res.statusCode = result.statusCode;
+				res.end(result.text);
+				return;
+			}
+
+			var item = '';
+			req.setEncoding('utf8');	
+			req.on('data', function ( chunk ) {
+				item += chunk;
+			});
+			req.on('end', function () {
+				var isUpdate = updateItems( result.path, item );
+				if( isUpdate ) {
+					res.statusCode = result.statusCode;
+					res.end( result.text );
+				}else {
+					res.statusCode = 404;
+					res.end('item is not found');
+				}
+			});
 			break;
 	}
 });
