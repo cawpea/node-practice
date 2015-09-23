4683f3202696d7ad9bde2d6c102d781eee212927
ms.oosumi <masaki.ohsumi.1021@outlook.jp>
2015-09-23 18:32:43 +0900

    add restful api(post & get)

--- http_server/http_server.js
+++ http_server/http_server.js
@@ -1,13 +1,26 @@
 var http = require('http');
+var url = require('url');
+var items = [];
 
-var server = http.createServer(function ( req, res ) {
-	var url = 'http://google.com';
-	var body = '<p>Redirecting to <a href="' + url + '">' + url + '</p>';
-
-	res.setHeader('Location', url);
-	res.setHeader('Content-Length', body.length);
-	res.setHeader('Content-Type', 'text/html');
-	res.statusCode = 302;
-	res.end(body);
+var server = http.createServer(function (req, res) {
+	switch( req.method ) {
+		case 'POST':
+			var item = '';
+			req.setEncoding('utf8');
+			req.on('data', function (chunk) {
+				item += chunk;
+			});
+			req.on('end', function () {
+				items.push(item);
+				res.end('OK\n');
+			});
+			break;
+		case 'GET':
+			items.forEach(function (item, i) {
+				res.write(i + ') ' + item + '\n');
+			});
+			res.end();
+			break;
+	}
 });
 server.listen(3000);
