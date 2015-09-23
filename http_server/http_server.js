75d66e3a928b1e96933ba4335538c664fb57b6f8
ms.oosumi <masaki.ohsumi.1021@outlook.jp>
2015-09-23 18:02:01 +0900

    add http response header.

--- http_server/http_server.js
+++ http_server/http_server.js
@@ -1,7 +1,13 @@
 var http = require('http');
 
 var server = http.createServer(function ( req, res ) {
-	res.write('Hello World');
-	res.end();
+	var url = 'http://google.com';
+	var body = '<p>Redirecting to <a href="' + url + '">' + url + '</p>';
+
+	res.setHeader('Location', url);
+	res.setHeader('Content-Length', body.length);
+	res.setHeader('Content-Type', 'text/html');
+	res.statusCode = 302;
+	res.end(body);
 });
 server.listen(3000);
