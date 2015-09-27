948d594d015254467f4ff561a73ad679fdf62ebb
ms.oosumi <masaki.ohsumi.1021@outlook.jp>
2015-09-27 16:11:06 +0900

    add not found error pattern to filesever.js

--- fileserver/fileserver.js
+++ fileserver/fileserver.js
@@ -8,12 +8,27 @@ var root = __dirname;
 var server = http.createServer(function (req, res) {
 	var url = parse(req.url);
 	var path = join( root, url.pathname );
-	var stream = fs.createReadStream(path);
-	stream.pipe(res);
 
-	stream.on('error', function (err) {
-		res.statusCode = 500;
-		res.end('Internal Server Error');
+	//ファイルが存在するか確認する
+	fs.stat(path, function (err, stat) {
+		if( err ) {
+			if('ENOENT' == err.code) {
+				res.statusCode = 404;
+				res.end('Not Found');
+			}else {
+				res.statusCode = 500;
+				res.end('Internal Server Error');
+			}
+		}else {
+			res.setHeader('Content-Length', stat.size);
+
+			var stream = fs.createReadStream(path);
+			stream.pipe(res);
+			stream.on('error', function (err) {
+				res.statusCode = 500;
+				res.end('Internal Server Error');
+			});
+		}
 	});
 });
 server.listen(3000);
