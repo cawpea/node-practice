7f528af940c34ae760a37877c7b2d3a25d5d8dd1
ms.oosumi <masaki.ohsumi.1021@outlook.jp>
2015-11-14 19:22:08 +0900

    add serve-index with connect in sample

--- connect/static.js
+++ connect/static.js
@@ -1,7 +1,10 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var serveIndex = require('serve-index');
 var compress = require('compression');
 
 var app = connect()
 	.use(compress())
-	.use(serveStatic( __dirname + '/html', {'index': ['default.html']})).listen(3000);
+	.use(serveIndex( __dirname + '/html' ))
+	.use(serveStatic( __dirname + '/html'))
+	.listen(3000);
