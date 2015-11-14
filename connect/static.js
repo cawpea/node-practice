e3828a77853e4a9b31c507582ea07f885ec7ba65
ms.oosumi <masaki.ohsumi.1021@outlook.jp>
2015-11-14 19:04:51 +0900

    add compression with connect in sample

--- connect/static.js
+++ connect/static.js
@@ -1,6 +1,7 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var compress = require('compression');
 
-var serve = serveStatic( __dirname + '/html', {'index': ['default.html']});
 var app = connect()
-	.use(serve).listen(3000);
+	.use(compress())
+	.use(serveStatic( __dirname + '/html', {'index': ['default.html']})).listen(3000);
